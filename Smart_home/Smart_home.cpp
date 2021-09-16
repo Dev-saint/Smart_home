287e80595b50a62aaea073e83ccaf7f9857915e3
Stanislav Cherkasov <chstas2002@gmail.com>
2021-09-16 12:27:23 +0700

    Start

---
+++ Smart_home/Smart_home.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <Windows.h>
+#include <string.h>
+
+using namespace std;
+
+struct kitchen
+{
+    float air_temp;
+    float air_hum;
+    int coffee;
+    float conc_co2;
+    int vent_speed;
+    int brightness;
+    int light;
+};
+
+struct bedroom
+{
+
+};
+
+struct bathroom
+{
+    float water_temp;
+    float air_temp;
+    float air_hum;
+};
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+    system("cls");
+}
